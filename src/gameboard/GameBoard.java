cefe732daa51fd4088741e4eb830e3c7cf974af5
Dimitar216 <omgweee74@gmail.com>
2021-02-04 00:22:26 +0200

    Added modal class to render a window when needed and refactored generation for tiles and added movement for starting tile

--- src/gameboard/GameBoard.java
+++ src/gameboard/GameBoard.java
@@ -1,75 +1,171 @@
 package gameboard;
 
-import tiles.GpsTile;
-import tiles.StartingTile;
 import tiles.Tile;
-import tiles.UninhabitableTile;
+import ui.Modal;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.util.concurrent.ThreadLocalRandom;
 
-public class GameBoard extends JFrame {
+public class GameBoard extends JFrame implements MouseListener {
     private final int startingPosition = ThreadLocalRandom.current().nextInt(1,5);
-    private Tile[][] tileCollection = new Tile[8][8];
+    private final Tile[][] tileCollection = new Tile[8][8];
     private final int TILE_SIZE = 100;
+    private Tile selectedTile;
+
     public GameBoard(){
+        StartTileInitializer();
+        gpsTilesGenerator();
+        uninhabitableTilesGenerator();
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setSize(800, 800);
         this.setVisible(true);
+        this.addMouseListener(this);
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        int row = this.getBoardCoordinates(e.getY());
+        int col = this.getBoardCoordinates(e.getX());
+        if(row<=7&&col<=7){
+            if(this.selectedTile !=null){
+                Tile tile = selectedTile;
+                if(!isUninhabitable(row,col)){
+                    startTileMovement(row, col, tile);
+                    return;
+                } else {
+                    Modal.render(this,"Warning!","Cannot enter uninhabitable area");
+                }
+            }
+            if(this.hasBoardTile(row,col)){
+                Tile tile = getBoardTile(row,col);
+                if(tile.getColor().equals(Color.WHITE)){
+                    this.selectedTile = this.getBoardTile(row,col);
+                }else {Modal.render(this,"Warning!","You can select only the white tile"); }
+            }else{Modal.render(this,"Warning!","Nothing to select.");}
+        }else {Modal.render(this,"Warning!","Out of bounds");}
+    }
+
+    private void startTileMovement(int row, int col, Tile tile) {
+        int initialRow = tile.getRow();
+        int initialCol = tile.getCol();
+        tile.move(row, col);
+        this.tileCollection[tile.getRow()][tile.getCol()] = this.selectedTile;
+        this.tileCollection[initialRow][initialCol] = null;
+        this.selectedTile = null;
+        this.repaint();
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
     }
     @Override
     public void paint(Graphics g){
         for(int row = 0; row < 8;row++){
             for(int col = 0; col < 8;col++){
-                TileChooser(g, row, col);
+                tileRenderer(g,row,col);
             }
         }
-        gpsTilesGenerator(g);
-        uninhabitableTilesGenerator(g);
-    }
-
-    private void TileChooser(Graphics g, int row, int col) {
-        if(row == 0 && col == 0 && startingPosition == 1){ StartingTile startingTile = new StartingTile(row, col,TILE_SIZE,TILE_SIZE);
-            startingTile.render(g);
-            tileCollection[row][col] = startingTile;
-        }else if(row == 7 && col == 0 && startingPosition == 2){ StartingTile startingTile = new StartingTile(row, col,TILE_SIZE,TILE_SIZE);
-            startingTile.render(g);
-            tileCollection[row][col] = startingTile;
-        }else if(row == 0 && col == 7 && startingPosition == 3){ StartingTile startingTile = new StartingTile(row, col,TILE_SIZE,TILE_SIZE);
-            startingTile.render(g);
-            tileCollection[row][col] = startingTile;
-        }else if(row == 7 && col == 7 && startingPosition == 4){ StartingTile startingTile = new StartingTile(row, col,TILE_SIZE,TILE_SIZE);
-            startingTile.render(g);
-            tileCollection[row][col] = startingTile;
-        } else{ Tile tile = new Tile(row, col,TILE_SIZE,TILE_SIZE);
-            tile.render(g);
+        for(int row = 0; row < 8;row++){
+            for(int col = 0; col < 8;col++){
+                if(this.tileCollection[row][col] == null) {
+                    Tile tile = new Tile(row, col, TILE_SIZE, TILE_SIZE, Color.GREEN);
+                    tile.render(g);
+                }
+            }
         }
     }
-    private void uninhabitableTilesGenerator(Graphics g){
+
+    private void StartTileInitializer() {
+        if(startingPosition == 1){
+            Tile startingTile = new Tile(0, 0,TILE_SIZE,TILE_SIZE,Color.WHITE);
+            tileCollection[0][0] = startingTile;
+        }else if(startingPosition == 2){
+            Tile startingTile = new Tile(0, 0,TILE_SIZE,TILE_SIZE,Color.WHITE);
+            tileCollection[7][0] = startingTile;
+        }else if(startingPosition == 3){
+            Tile startingTile = new Tile(0, 0,TILE_SIZE,TILE_SIZE,Color.WHITE);
+            tileCollection[0][7] = startingTile;
+        }else if(startingPosition == 4){
+            Tile startingTile = new Tile(0, 0,TILE_SIZE,TILE_SIZE,Color.WHITE);
+            tileCollection[7][7] = startingTile;
+        }
+    }
+    private void uninhabitableTilesGenerator(){
         for(int i = 0; i<5;i++){
             int randomCoordinateRow = ThreadLocalRandom.current().nextInt(0,8);
             int randomCoordinateCol = ThreadLocalRandom.current().nextInt(0,8);
             if(tileCollection[randomCoordinateRow][randomCoordinateCol] == null){
-                UninhabitableTile uninhabitableTile = new UninhabitableTile(randomCoordinateRow,randomCoordinateCol,TILE_SIZE,TILE_SIZE);
-                uninhabitableTile.render(g);
+                Tile uninhabitableTile = new Tile(randomCoordinateRow,randomCoordinateCol,TILE_SIZE,TILE_SIZE,Color.BLUE);
                 tileCollection[randomCoordinateRow][randomCoordinateCol] = uninhabitableTile;
             } else {
                 i--;
             }
         }
     }
-    private void gpsTilesGenerator(Graphics g){
+    private void gpsTilesGenerator(){
         for(int i = 0; i<8;i++){
             int randomCoordinateRow = ThreadLocalRandom.current().nextInt(0,8);
             int randomCoordinateCol = ThreadLocalRandom.current().nextInt(0,8);
             if(tileCollection[randomCoordinateRow][randomCoordinateCol] == null){
-                GpsTile gpsTile = new GpsTile(randomCoordinateRow,randomCoordinateCol,TILE_SIZE,TILE_SIZE);
-                gpsTile.render(g);
+                Tile gpsTile = new Tile(randomCoordinateRow,randomCoordinateCol,TILE_SIZE,TILE_SIZE,Color.RED);
                 tileCollection[randomCoordinateRow][randomCoordinateCol] = gpsTile;
             } else {
                 i--;
             }
         }
+    }
+    private void tileRenderer(Graphics g,int row,int col){
+        if(this.hasBoardTile(row,col)){
+            Tile tile = getBoardTile(row,col);
+            if(tile.getColor().equals(Color.BLUE)){
+                Tile uninhabitableTile = new Tile(row,col,TILE_SIZE,TILE_SIZE,Color.BLUE);
+                uninhabitableTile.render(g);
+            } else if(tile.getColor().equals(Color.RED)){
+                Tile gpsTile = new Tile(row,col,TILE_SIZE,TILE_SIZE,Color.RED);
+                gpsTile.render(g);
+            } else if(tile.getColor().equals(Color.WHITE)){
+                Tile startingTile = new Tile(row,col,TILE_SIZE,TILE_SIZE,Color.WHITE);
+                startingTile.render(g);
+            }
+        }
+    }
+    private int getBoardCoordinates(int coordinates){
+        return  coordinates/TILE_SIZE;
+    }
+
+    private Tile getBoardTile(int row, int col){
+        return this.tileCollection[row][col];
+    }
+
+
+    private boolean hasBoardTile(int row,int col){
+        return this.getBoardTile(row,col) !=null;
+    }
+
+    private boolean isUninhabitable(int row,int col){
+        if(hasBoardTile(row,col)){
+            Tile tile = getBoardTile(row,col);
+            return tile.getColor().equals(Color.BLUE);
+        }
+        return false;
     }
 }
--- src/tiles/Tile.java
+++ src/tiles/Tile.java
@@ -4,10 +4,11 @@ import java.awt.*;
 import java.awt.geom.Line2D;
 
 public class Tile {
-    protected int row;
-    protected int col;
-    protected int widthOfTile;
-    protected int heightOfTile;
+    private int row;
+    private int col;
+    private final int widthOfTile;
+    private final int heightOfTile;
+    private final Color color;
 
     /**
      * Constructor for Tile
@@ -16,11 +17,12 @@ public class Tile {
      * @param widthOfTile width of tile
      * @param heightOfTile height of tile
      */
-    public Tile(int row, int col,int widthOfTile,int heightOfTile){
+    public Tile(int row, int col,int widthOfTile,int heightOfTile,Color color){
         this.row          = row;
         this.col          = col;
         this.widthOfTile  = widthOfTile;
         this.heightOfTile = heightOfTile;
+        this.color = color;
     }
 
     /**
@@ -34,13 +36,13 @@ public class Tile {
         int tileX =  (this.col * this.widthOfTile);
         int tileY =  (this.row * this.heightOfTile);
 
-        g.setColor(Color.GREEN);
+        g.setColor(this.color);
         g.fillRect(tileX,tileY,this.widthOfTile, this.heightOfTile);
 
         outlineRenderer(g, lineDrawer, tileX, tileY);
     }
 
-    protected static void outlineRenderer(Graphics g, Graphics2D lineDrawer, int tileX, int tileY) {
+    private static void outlineRenderer(Graphics g, Graphics2D lineDrawer, int tileX, int tileY) {
         g.setColor(Color.BLACK);
         Line2D line0 = new Line2D.Float(tileX, tileY, tileX, tileY +99);
         Line2D line1 = new Line2D.Float(tileX +99, tileY, tileX +99, tileY +99);
@@ -54,5 +56,28 @@ public class Tile {
         lineDrawer.draw(line2);
         //bottom
         lineDrawer.draw(line3);
+    }
+
+    public void move(int row,int col){
+        this.row = row;
+        this.col = col;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+    public boolean isMoveValid(int moveRow,int moveCol){
+        int rowCoefficient =  Math.abs(moveRow-this.row);
+        int colCoefficient =  Math.abs(moveCol - this.col);
+
+        return rowCoefficient == 0 && colCoefficient == 1 || rowCoefficient == 1 && colCoefficient == 0;
     }
 }
